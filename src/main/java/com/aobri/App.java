8967113fb82d8a2b4d428a6cb5c83fb7e8128b2c
aobri <ahmed.o.nabri@gmail.com>
2022-02-15 23:56:00 -0600

    Add steps and create rendering service interface

--- src/main/java/com/aobri/App.java
+++ src/main/java/com/aobri/App.java
@@ -3,6 +3,10 @@ package com.aobri;
 public class App {
 
     public static void main(String[] args) {
+        // configure window for display
 
+        // Read csv file for long-lat points
+
+        // render points on image for each csv PostalCoordinate
     }
 }
---
+++ src/main/java/com/aobri/service/RenderingService.java
@@ -0,0 +1,4 @@
+package com.aobri.service;
+
+public interface RenderingService {
+}
