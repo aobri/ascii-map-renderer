47f72d05f57f75722df6a3c0cbd3b05129411e50
aobri <ahmed.o.nabri@gmail.com>
2022-02-17 13:54:43 -0600

    Add Borders class

---
+++ src/main/java/com/aobri/model/Borders.java
@@ -0,0 +1,51 @@
+package com.aobri.model;
+
+public class Borders {
+
+    private int bottom;
+    private int left;
+    private int right;
+    private int top;
+
+    public Borders() {
+    }
+
+    public Borders(int left, int top, int right, int bottom) {
+        this.left = left;
+        this.top = top;
+        this.right = right;
+        this.bottom = bottom;
+    }
+
+    public int getLeft() {
+        return left;
+    }
+
+    public void setLeft(int left) {
+        this.left = left;
+    }
+
+    public int getTop() {
+        return top;
+    }
+
+    public void setTop(int top) {
+        this.top = top;
+    }
+
+    public int getRight() {
+        return right;
+    }
+
+    public void setRight(int right) {
+        this.right = right;
+    }
+
+    public int getBottom() {
+        return bottom;
+    }
+
+    public void setBottom(int bottom) {
+        this.bottom = bottom;
+    }
+}
